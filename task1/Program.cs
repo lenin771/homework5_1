0d6a93ac8cb6be7b11e488d043e9dbd65b02a179
Your Name <you@example.com>
2022-10-23 14:27:31 +0300

    add cod

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,27 @@
 ﻿// программf, которая из имеющегося массива строк 
 // формирует массив из строк, длина которых меньше либо равна 3 символа
 
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] mas1 = new string[3,1];
+            mas1[0,0] = "hello";
+            mas1[1,0] = "2";
+            mas1[2,0] = "world";
+            string[] mas2 = new string [3];
+            for (int i=0; i<=2; i++)
+            {
+                mas2[i] = mas1 [i,0];
+            }
+            
+            Console.Write($"{mas2[0]}, {mas2[1]}, {mas2[2]}");
+            Console.WriteLine();
+        }
+    }
+}
+
